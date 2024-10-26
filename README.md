dead2606ddaacb0bee41c638c670cff77198b271
snjyor <snjyor@163.com>
2024-10-26 08:59:13 +0800

    readme.md

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# learncode_frontend
+# LearnCode
+
+LearnCode是一个项目驱动、需求导向的编程学习平台。通过实际项目和需求,帮助学习者从零开始掌握新的编程语言。
+
+## 技术栈
+HTML, CSS, JavaScript
+
+## 特点
+
+- 项目驱动学习
+- 内置代码编辑器
+- 在线调试功能  
+- 逐步引导教学
+
+## 用户管理
+
+用户活跃度，学习进度，学习动态，签到，积分，等级，徽章，成就，邀请
+
+## 编程语言课程
+
+- Python
+- JavaScript
+- TypeScript 
+- Golang
+- Rust
+- Solidity
+- HTML & CSS
+- 等等
+
+## 主要页面
+
+1. 我的学习
+2. 练习页面
+3. 项目探索
+4. 文档库
+5. 个人中心
+
+## 项目页面
+#### 主页
+![主页](./sources/images/readme/1.png)
+#### 我的学习
+![我的学习](./sources/images/readme/2.png)
+#### 练习页面
+![练习页面](./sources/images/readme/3.png)
+#### 项目探索
+![项目探索](./sources/images/readme/4.png)
+#### 文档库
+![文档库](./sources/images/readme/5.png)
+#### 个人中心
+![个人中心](./sources/images/readme/6.png)
--- sources/js/utils.js
+++ sources/js/utils.js
@@ -9,3 +9,31 @@ function getRandomNumber(min, max) {
 }
 
 // 可以根据需要添加更多工具函数
+const arr = [
+    {name: 'a', age: 10},
+    {name: 'b', age: 12},
+    {name: 'c', age: 30},
+    {name: 'd', age: 46},
+    {name: 'e', age: 35},
+    {name: 'f', age: 62},
+    {name: 'g', age: 47},
+    {name: 'h', age: 39},
+]
+
+const newArr = arr.sort((a,b)=>a.age-b.age);
+console.log(newArr);
+
+function customSort(arr, compFunc) {
+    for (let i=0; i< arr.length-1;i++){
+        for (let j=0;j<arr.length-1-i;j++){
+            if (compFunc(arr[j], arr[j+1])>0){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+customSort(arr, (a,b)=>a.age-b.age);
+console.log(arr);
