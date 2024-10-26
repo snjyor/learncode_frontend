0d53cead7a7740ec73b52ec2aeab010db9a73808
Jeffrey Yang <snjyor@163.com>
2024-10-26 08:39:05 +0800

    del 2 log

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = 5500;
 const dotenv = require('dotenv');
-console.log('env:', dotenv.config().parsed);
 
 // 静态文件服务
 app.use(express.static(path.join(__dirname)));
 
 // API 路由
 app.get('/projects/mine', (req, res) => {
-    console.log("现在在这里，请注意，这是一个API路由");
     const userId = req.query.user_id;
     // 这里处理项目数据的逻辑
     res.json({ projects: [/* 项目数据 */] });
